a2efe13624a5e37b3a2ea7d2fe5b4038d56c8179
KeaiPandas <zkphao3@gmail.com>
2022-01-28 18:36:30 +0800

    Update 原型链继承的几种方式.js

--- js/原型链继承的几种方式.js
+++ js/原型链继承的几种方式.js
@@ -1,25 +1,49 @@
-function Parent() {
-  this.name = "parent"
+function Parent(name, id) {
+  this.name = name
+  this.id = id
   this.list = ['a']
+  this.printName = function () {
+    console.log(this.name)
+  }
 }
 
-Parent.prototype.sayHi = function () {
-  console.log('hi')
+Parent.prototype.sayName = function () {
+  console.log(this.name)
 }
 
-function Child() {
-  
+function Child(name, id) {
+  Parent.call(this, name, id)
 }
 
 Child.prototype = new Parent()
 
-var child = new Child()
-console.log(child.name)
-child.sayHi()
-console.log(child.__proto__)
+var child = new Child('小b娃娃', 1)
+child.printName()
+child.sayName()
 
 var a = new Child()
 var b = new Child()
-
 a.list.push('b')
 console.log(b.list)
+console.log(a.list)
+
+// 原型式继承
+var parent = {
+  names: ['a']
+}
+function copy(object) {
+  function F() {}
+  F.prototype = object
+  return new F()
+}
+
+var child = copy(parent)
+
+function createObject(obj) {
+  var o = copy(obj)
+  o.getNames = function() {
+    console.log(this.names)
+    return this.names
+  }
+  return o
+}
