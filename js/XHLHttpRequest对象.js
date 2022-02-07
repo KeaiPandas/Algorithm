95b838149a4aba4e3585d0f45f76cd930e9d1755
KeaiPandas <zkphao3@gmail.com>
2022-02-07 14:19:30 +0800

    Create XHLHttpRequest对象.js

---
+++ js/XHLHttpRequest对象.js
@@ -0,0 +1,31 @@
+let xhr = new XMLHttpRequest()
+
+xhr.open("get", "example/php?name1=value1&name2=value2", true)
+
+function addURLParam(url, name, value) {
+  url += (url.indexOf("?" == -1 ? "?" : "&"))
+  url += encodeURIComponent(name) + "=" + encodeURIComponent(value)
+  return url
+}
+
+let xhr = new XMLHttpRequest()
+xhr.onreadystatechange = function() {
+  if (xhr.readyState === 4) {
+    try {
+      if ((xhr.status >= 200 && xhr.status < 300) || xhr.status == 304) {
+        alert(xhr.responseText)
+      } else {
+        alert("Request was unsucessful: " + xhr.status)
+      }
+    }
+    catch(ex) {
+
+    }
+  }
+}
+
+xhr.open("get", "timeout.php", true)
+xhr.timeout = 1000
+xhr.ontimeout = function() {
+  alert("Request did not return in a second.")
+}
